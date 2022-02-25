0b93c8585235c22eb4f77d5ba44edb8561a9d4ac
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-02-25 23:24:04 +0100

    update

--- src/components/ShopItems.tsx
+++ src/components/ShopItems.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 
 interface itemProps {
-    items: {id:number; name:string; description:string}[]
+    items: {id:number; name:string; description:string}
 }
 
 const shopItems: React.FC<itemProps> = ({items}) => {
