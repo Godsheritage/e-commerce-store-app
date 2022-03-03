83fe022a9ffba269b9c6f9974f340cdc3513e8cb
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-03-03 17:02:00 +0100

    mobile responsiveness

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,34 +1,77 @@
-import React from 'react'
+import React from "react";
 
 const Main = () => {
   return (
-    <div id="carouselExampleIndicators" className ="carousel slide" data-bs-ride="carousel">
-  <div className ="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className ="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div className ="carousel-inner">
-    <div className ="carousel-item active">
-      <img src="https://www.shoelayers.com/image/cache/catalog/slideshow/shoelayers/merrell_continum-1650x550.png" className ="d-block w-100" alt="..."></img>
+    <div
+      id="carouselExampleIndicators"
+      className="carousel slide"
+      data-bs-ride="carousel"
+    >
+      <div className="carousel-indicators">
+        <button
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide-to="0"
+          className="active"
+          aria-current="true"
+          aria-label="Slide 1"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide-to="1"
+          aria-label="Slide 2"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide-to="2"
+          aria-label="Slide 3"
+        ></button>
+      </div>
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img
+            src="https://www.shoelayers.com/image/cache/catalog/slideshow/shoelayers/merrell_continum-1650x550.png"
+            className="d-block w-100"
+            alt="..."
+          ></img>
+        </div>
+        <div className="carousel-item">
+          <img
+            src="https://www.shoelayers.com/image/cache/catalog/slideshow/shoelayers/cat-boot-1-1650x550.png"
+            className="d-block w-100"
+            alt="..."
+          ></img>
+        </div>
+        <div className="carousel-item">
+          <img
+            src="https://www.shoelayers.com/image/cache/catalog/slideshow/shoelayers/merrell-1650x550.png"
+            className="d-block w-100"
+            alt="..."
+          ></img>
+        </div>
+      </div>
+      <button
+        className="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExampleIndicators"
+        data-bs-slide="prev"
+      >
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button
+        className="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExampleIndicators"
+        data-bs-slide="next"
+      >
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
     </div>
-    <div className ="carousel-item">
-      <img src="https://www.shoelayers.com/image/cache/catalog/slideshow/shoelayers/cat-boot-1-1650x550.png" className ="d-block w-100" alt="..."></img>
-    </div>
-    <div className ="carousel-item">
-      <img src="https://www.shoelayers.com/image/cache/catalog/slideshow/shoelayers/merrell-1650x550.png" className ="d-block w-100" alt="..."></img>
-    </div>
-  </div>
-  <button className ="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-    <span className ="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span className ="visually-hidden">Previous</span>
-  </button>
-  <button className ="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-    <span className ="carousel-control-next-icon" aria-hidden="true"></span>
-    <span className ="visually-hidden">Next</span>
-  </button>
-</div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
