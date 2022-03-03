bb55b4bf817e2404f9d8daca73ed9ee882f68429
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-03-03 16:55:14 +0100

    updates

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,7 +2,32 @@ import React from 'react'
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <div id="carouselExampleIndicators" className ="carousel slide" data-bs-ride="carousel">
+  <div className ="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className ="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div className ="carousel-inner">
+    <div className ="carousel-item active">
+      <img src="..." className ="d-block w-100" alt="..."></img>
+    </div>
+    <div className ="carousel-item">
+      <img src="..." className ="d-block w-100" alt="..."></img>
+    </div>
+    <div className ="carousel-item">
+      <img src="..." className ="d-block w-100" alt="..."></img>
+    </div>
+  </div>
+  <button className ="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span className ="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className ="visually-hidden">Previous</span>
+  </button>
+  <button className ="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span className ="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className ="visually-hidden">Next</span>
+  </button>
+</div>
   )
 }
 
