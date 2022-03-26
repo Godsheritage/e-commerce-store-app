95543b837e6f1d8d4ea8e74683882dcd13d574fd
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-03-26 15:15:41 +0100

    updates

--- server.js
+++ server.js
@@ -91,11 +91,12 @@ const cartItems = [];
 // const singleProduct = [];
 
 app.use((req, res, next) => {
-  res.append("Access-Control-Allow-Origin", ["*"]);
-  res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.append("Access-Control-Allow-Headers", "Content-Type");
-  next();
+  res.append('Access-Control-Allow-Origin', ['*'])
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  res.append('Access-Control-Allow-Headers', 'Content-Type')
+  next()
 });
+
 
 app.use(express.json());
 
