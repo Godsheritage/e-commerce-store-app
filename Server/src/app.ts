2bf243d1947e08ad59f60378d9d9c76f7cef7522
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-03-27 11:09:03 +0100

    updates

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+import morgan from "morgan";
 // import cors from 'cors'
 import cartRoutes from "./routes/Cart Routes/cart.routes";
 import productDataRoutes from "./routes/Product Routes/products.routes";
@@ -14,14 +15,15 @@ app.use((req, res, next) => {
 });
 
 // app.use(cors())
+app.use(morgan("combined"));
+
+app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public ", "index.html"));
 });
-
-app.use(express.json());
 
 app.use("/productData", productDataRoutes);
 app.use("/cartItems", cartRoutes);
