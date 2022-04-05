a58e2b6f1569830d86a71aa728ad925b8697f9bb
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-04-05 14:41:17 +0100

    added framer motion to front end

--- Client/src/pages/Cart.tsx
+++ Client/src/pages/Cart.tsx
@@ -1,11 +1,11 @@
-import React, { useContext} from "react";
+import React, { useContext } from "react";
 import ProductContext from "../context/ProductContext";
 import { contextTypes } from "../shared/types";
 import { FaTimes } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Cart: React.FC = () => {
-
   // importing items from useContext API
   const { removeItem, sum, checkout, cartItems } = useContext(
     ProductContext
@@ -22,28 +22,37 @@ const Cart: React.FC = () => {
 
   return (
     <>
-      {cartItems.map((cart) => (
-        <div className="cartItems" key={cart.id}>
-          <img className="img" src={cart.image} alt="CartItem" />
-          <p className="card-title">{cart.name}</p>
-          <p className=" itemsPrice">${cart.price}</p>
-          <FaTimes
-            className="faTimes"
-            color="#2d3261"
-            onClick={() => removeItem(cart.id)}
-            />
+      <AnimatePresence>
+        {cartItems.map((cart) => (
+          <motion.div
+           key={cart.id}
+           initial = {{ opacity : 0}}
+           animate = {{opacity : 1}}
+           exit = {{opacity : 0}}
+           >
+            <div className="cartItems" key={cart.id}>
+              <img className="img" src={cart.image} alt="CartItem" />
+              <p className="card-title">{cart.name}</p>
+              <p className=" itemsPrice">${cart.price}</p>
+              <FaTimes
+                className="faTimes"
+                color="#2d3261"
+                onClick={() => removeItem(cart.id)}
+              />
+            </div>
+          </motion.div>
+        ))}
+        <div>
+          <hr />
+          <div className="d-flex justify-content-between">
+            <p className="text-right pt-2">Total: ${sum}</p>
+            <button className="btn btn-primary btn-sm" onClick={checkout}>
+              {" "}
+              Checkout
+            </button>
+          </div>
         </div>
-      ))}
-      <div>
-        <hr />
-        <div className="d-flex justify-content-between">
-          <p className="text-right pt-2">Total: ${sum}</p>
-          <button className="btn btn-primary btn-sm" onClick={checkout}>
-            {" "}
-            Checkout
-          </button>
-        </div>
-      </div>
+      </AnimatePresence>
     </>
   );
 };
