ce85dc15df56fdafee6a0a01ae31f7fc9f509a8e
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-04-14 12:21:33 +0100

    updates

--- Server/dist/app.js
+++ Server/dist/app.js
@@ -9,6 +9,7 @@ const path_1 = __importDefault(require("path"));
 const helmet_1 = __importDefault(require("helmet"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const morgan_1 = __importDefault(require("morgan"));
+const passport_1 = __importDefault(require("passport"));
 const express_1 = __importDefault(require("express"));
 const cart_routes_1 = __importDefault(require("./routes/Cart Routes/cart.routes"));
 const products_routes_1 = __importDefault(require("./routes/Product Routes/products.routes"));
@@ -18,15 +19,22 @@ const config = {
     CLIENT_ID: process.env.CLIENT_ID,
     CLIENT_SECRET: process.env.CLIENT_SECRET,
 };
-exports.app.use((0, helmet_1.default)());
-//for cross origin resoursce sharing
-exports.app.use((0, cors_1.default)());
-const checkLoggedIn = () => {
+const checkLoggedIn = (req, res, next) => {
     const isLoggedIn = true;
+    if (!isLoggedIn) {
+        return res.status(400).json({
+            error: "You must be logged in first",
+        });
+    }
+    next();
 };
+exports.app.use((0, helmet_1.default)());
+exports.app.use((0, cors_1.default)());
+exports.app.use(passport_1.default.initialize());
+//for cross origin resoursce sharing
 exports.app.use((0, morgan_1.default)("combined"));
-exports.app.get('auth.google', (req, res) => { });
-exports.app.get('auth.google/callback', (req, res) => { });
+exports.app.get("auth.google", (req, res) => { });
+exports.app.get("auth.google/callback", (req, res) => { });
 exports.app.use(express_1.default.json());
 exports.app.use("/productData", products_routes_1.default);
 exports.app.use("/cartItems", cart_routes_1.default);
--- Server/src/app.ts
+++ Server/src/app.ts
@@ -3,9 +3,9 @@ import path from "path";
 import helmet from "helmet";
 import dotenv from "dotenv";
 import morgan from "morgan";
-import passport from 'passport'
-import {Strategy} from 'passport-google-oauth20'
-import express from "express";
+import passport from "passport";
+import { Strategy } from "passport-google-oauth20";
+import express, { RequestHandler } from "express";
 import cartRoutes from "./routes/Cart Routes/cart.routes";
 import productDataRoutes from "./routes/Product Routes/products.routes";
 
@@ -13,27 +13,33 @@ export const app = express();
 
 dotenv.config();
 
-
 const config = {
   CLIENT_ID: process.env.CLIENT_ID,
   CLIENT_SECRET: process.env.CLIENT_SECRET,
 };
 
+const checkLoggedIn: RequestHandler = (req, res, next) => {
+  const isLoggedIn = true;
+  if (!isLoggedIn) {
+    return res.status(400).json({
+      error: "You must be logged in first",
+    });
+  }
+  next();
+};
 
 app.use(helmet());
-
-//for cross origin resoursce sharing
 app.use(cors());
 
-const checkLoggedIn = () => {
-  const isLoggedIn = true
-}
+app.use(passport.initialize());
+
+//for cross origin resoursce sharing
 
 app.use(morgan("combined"));
 
-app.get('auth.google', (req , res) => {} )
+app.get("auth.google", (req, res) => {});
 
-app.get('auth.google/callback', (req , res) => {} )
+app.get("auth.google/callback", (req, res) => {});
 
 app.use(express.json());
 
